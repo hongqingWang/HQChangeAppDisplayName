846e70c290eb583445c0bb559cda3c60352e3687
hongqingWang <272338444@qq.com>
2017-02-07 12:23:40 +0800

    Initial Commit

---
+++ HQChangeAppDisplayName/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HQChangeAppDisplayName
+//
+//  Created by 王红庆 on 2017/2/7.
+//  Copyright © 2017年 王红庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
